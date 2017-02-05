dcfd1bfa243c76b7138ff1a1182e3b3ae5202ea5
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-02-05 22:02:58 +0530

    Stage I Complete

--- EE210 - Assignment 1/convolve.py
+++ EE210 - Assignment 1/convolve.py
@@ -3,34 +3,34 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 
 dir = "Files/"
-FIR = "long_echo_hall_16k.wav"
-Source_Sound = "BheegiRegular-part.wav"
+results_dir = dir + "results/"
+FIR = "Five_Columns_Long_16k"
+Source_Sound = "BheegiRegular"
 
 if __name__ == '__main__':
-	sampling_IR, IR = wavfile.read(dir + FIR)
-	sampling_sound, input_sound = wavfile.read(dir + Source_Sound)
+	sampling_IR, IR = wavfile.read(dir + FIR + ".wav")
+	sampling_sound, input_sound = wavfile.read(dir + Source_Sound + ".wav")
+	if (sampling_sound == sampling_IR):
+		IR_L = IR[:, 0]
+		IR_R = IR[:, 1]
 
-	IR_L = IR[:, 0]
-	IR_R = IR[:, 1]
+		N = len(IR_L) + len(input_sound)
 
-	N = len(IR_L) + len(input_sound)
+		input_sound = list(input_sound)
+		IR_L = list(IR_L)
+		IR_R = list(IR_R)
 
-	input_sound = list(input_sound)
-	IR_L = list(IR_L)
-	IR_R = list(IR_R)
+		extend_domain(input_sound, N)
+		extend_domain(IR_L, N)
+		extend_domain(IR_R, N)
 
-	extend_domain(input_sound, N)
-	extend_domain(IR_L, N)
-	extend_domain(IR_R, N)
+		X = np.asarray(fft(input_sound))
+		IR = np.asarray(fft(IR_L))
 
-	X = np.asarray(fft(input_sound))
-	IR = np.asarray(fft(IR_L))
+		Y = X * IR
+		y = ifft(Y)
 
-	Y = X * IR
-	y = ifft(Y)
+		yr = np.real(y)
+		yr /= max(yr) * 1.01
 
-	yr = np.real(y)
-	yr /= max(yr) * 1.01
-
-	wavfile.write('out1.wav', sampling_sound, yr)
-
+		wavfile.write(results_dir + FIR + "_complete.wav", sampling_sound, yr)
