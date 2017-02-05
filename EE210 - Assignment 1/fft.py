d81cea213c5fccc9e390d21f12dfbec6ba8ad53c
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-02-05 13:00:50 +0530

    Bleh

--- EE210 - Assignment 1/fft.py
+++ EE210 - Assignment 1/fft.py
@@ -10,21 +10,20 @@ def fft(x):
 
 	if (len(odd) < N//2 - 1):
 		print "Error"
-		break
 	T= [exp(-2j*pi*k/N)*odd[k] for k in range(N//2)]
 	return [even[k] + T[k] for k in range(N//2)] + \
 			[even[k] - T[k] for k in range(N//2)]
 
-# def ifft(x):
-# 	# Reusing code segments for logarithmic complexity
-# 	N = len(x)
-# 	if N <= 1: return x
-# 	even = fft(x[0::2])
-# 	odd =  fft(x[1::2])
-# 	if (len(odd) < N//2 - 1): print "Error"
-# 	T= [exp(2j*pi*k/N)*odd[k] for k in range(N//2)]
-# 	return [even[k] + T[k] for k in range(N//2)] + \
-# 			[even[k] - T[k] for k in range(N//2)]
+def ifft(x):
+	# Reusing code segments for logarithmic complexity
+	N = len(x)
+	if N <= 1: return x
+	even = fft(x[0::2])
+	odd =  fft(x[1::2])
+	if (len(odd) < N//2 - 1): print "Error"
+	T= [exp(2j*pi*k/N)*odd[k] for k in range(N//2)]
+	return [even[k] + T[k] for k in range(N//2)] + \
+			[even[k] - T[k] for k in range(N//2)]
 
 
 def FFT_vectorized(x):
