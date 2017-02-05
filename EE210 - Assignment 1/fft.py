145c414589e2e41a272ca19f90c03939a7e11e23
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-02-05 12:58:04 +0530

    FFT Dump

--- EE210 - Assignment 1/fft.py
+++ EE210 - Assignment 1/fft.py
@@ -7,7 +7,10 @@ def fft(x):
 	if N <= 1: return x
 	even = fft(x[0::2])
 	odd =  fft(x[1::2])
-	if (len(odd) < N//2 - 1): print "Error"
+
+	if (len(odd) < N//2 - 1):
+		print "Error"
+		break
 	T= [exp(-2j*pi*k/N)*odd[k] for k in range(N//2)]
 	return [even[k] + T[k] for k in range(N//2)] + \
 			[even[k] - T[k] for k in range(N//2)]
