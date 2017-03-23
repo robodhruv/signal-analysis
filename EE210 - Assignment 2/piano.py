19e687a45e1cfe3cd2c4bb561d76e940bd0f64f2
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-03-23 10:29:28 +0530

    Piano SR Complete

--- EE210 - Assignment 2/piano.py
+++ EE210 - Assignment 2/piano.py
@@ -2,14 +2,27 @@ from music_synth import *
 
 harmonics = np.array([1., - 1. / 9, 1. / 25])  # Spectral Profile
 rate = 16  # sampling rate in kHz
+sustain = 0.75
+
+def get_envelope(duration):
+	global rate
+	envelope = np.ones(sustain * duration * rate)
+	x = np.arange(duration * rate * (1 - sustain))
+	x = x / np.max(x)
+	release = 1 - x
+	# envelope = list(envelope)
+	envelope = np.append(envelope, release)
+	return envelope
 
 
 def gen_tone(f, duration):
+	global rate
 	t = np.arange(duration * rate)
 	x = np.sin(2 * np.pi * f * t / (1000 * rate))
 	x = x + harmonics[1] * np.sin(2 * np.pi * 3 * f * t / (1000 * rate))
 	x = x + harmonics[2] * np.sin(2 * np.pi * 5 * f * t / (1000 * rate))
 	return x
+
 def generate_song(filename):
 	strip_file(filename)
 	notes_file = get_notes(filename)
@@ -25,10 +38,12 @@ def generate_song(filename):
 	song = []
 	for i in range(len(notes)):
 		tone = gen_tone(float(notes[i][0]), int(notes[i][1]))
-		song.extend(list(tone))
+		envelope = get_envelope(int(notes[i][1]))
+		tune = tone * envelope
+		song.extend(list(tune))
 	song = np.clip(song, -1.0, 1.0)
 	return song
 
 
 song = generate_song("Songs/song_b.txt")
-sd.play(song, rate * 1000)
+# sd.play(song, rate * 1000)
